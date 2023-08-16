04a7d8150c152dbd9784f09b3f9628783a5205ac
zac-moon <zacemoon@gmail.com>
2023-08-16 18:35:27 +0100

    restarted all, init comit

--- client.py
+++ client.py
@@ -1,54 +1,46 @@
 import socket
-import tkinter as tk
 
-def loggedin(username, password):
-    loginwin = tk.Toplevel(root)
-    loginwin.title(f'ZBANK LINK - {username}')
-    loginwin.geometry('800x600')
-    def getBal():
-        client_socket.send(f"balance.{username}".encode())
-        bal = client_socket.recv(1024).decode()
-        client_socket.close()
-        return bal
-
-    balance = tk.StringVar()
-    balance.set(f'Balance : £{getBal()}') 
-
-    balanceLabel = tk.Label(loginwin, textvariable=balance, font=('Arial', 60))
-    balanceLabel.pack()
-
-root = tk.Tk()
-root.title("ZBANK LINK - LOGIN")
-root.geometry('800x600')
-
-server_host = socket.gethostbyname()
-server_port = 12345
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_host, server_port))
 
-def main():
-    username = usernameEntry.get()
-    password = passwordEntry.get()
-    client_socket.send(f'login.{username}.{password}'.encode())
-    data = client_socket.recv(1024).decode()
-    if data == '1':
-        usernameEntry.delete(0, tk.END)
-        passwordEntry.delete(0, tk.END)
-        loggedin(username, password)
+server_address = ('192.168.1.71', 12345)
+client_socket.connect(server_address)
+login = False
 
-loginTitle = tk.Label(root, text='Login To ZBANK LINK', font=('Arial', 40))
-usernameLabel = tk.Label(root, text='Username:')
-usernameEntry = tk.Entry(root)
-passwordLabel = tk.Label(root, text="Password:")
-passwordEntry = tk.Entry(root, show="*")
-loginButton = tk.Button(root, text="Login", command=main)
-
-loginTitle.pack()
-usernameLabel.pack()
-usernameEntry.pack()
-passwordLabel.pack()
-passwordEntry.pack()
-loginButton.pack()
-
-root.mainloop()
-client_socket.close()
+try:
+    while True:
+        for i in range(1, 64):
+            print('')
+        print('\n\nZBANK LINK LOGIN')
+        print("Enter Details to login: ")
+        username = input("Enter Username (or 'exit' to quit): ").strip()
+        if username == "exit":
+            break
+        password = input('Enter Password: ').strip()
+        client_socket.send(f'login.{username}.{password}'.encode('utf-8'))
+        resp = client_socket.recv(1024).decode('utf-8')
+        if resp == "1":
+            print("Correct Details - Logged In\n\n")
+            print('ENTER 0 TO LOGOUT \nENTER 1 FOR BALANCE \nENTER 2 FOR TRANSFER')
+            login = True
+            while login:
+                mode = input('Enter code: ')
+                if mode == '0':
+                    login = False
+                    client_socket.send('0 - Logged Out'.encode('utf-8'))
+                    break
+                elif mode == '1':
+                    client_socket.send(f'balance.{username}'.encode('utf-8'))
+                    bal = client_socket.recv(1024).decode('utf-8')
+                    print(f'Balance: {bal}')
+                elif mode == '2':
+                    amount = input('Enter amount to transfer (0 to exit): ').strip()
+                    to = input('Enter account username to transfer to (0 to cancel): ').strip()
+                    client_socket.send(f'transfer.{username}.{amount}.{to}'.encode('utf-8'))
+                    conf = client_socket.recv(1024).decode('utf-8')
+                    print(conf)
+        elif resp == '0':
+            print('Account Not Found - Please Sign Up Instead')
+        else:
+            print("Incorrect Details. Please try again.")
+finally:
+    client_socket.close()
--- server.py
+++ server.py
@@ -1,51 +1,85 @@
 import socket
-
-host = socket.gethostbyname()
-port = 12345      
+import datetime
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((host, port))
+
+server_address = ('192.168.1.71', 12345)
+server_socket.bind(server_address)
 server_socket.listen(1)
 
-print("Server listening on", host, "port", port)
+print('Server is listening on', server_address)
 
 while True:
+    print('Waiting for a connection...')
     client_socket, client_address = server_socket.accept()
-    print("Connection from:", client_address)
 
-    data = client_socket.recv(1024).decode() 
-    datas = data.split('.')
-    cmd = datas[0]
+    try:
+        print('Connection from', client_address)
+        while True:
+            data = client_socket.recv(1024)
+            if data:
+                data = data.decode('utf-8')
 
-    if cmd == "login":
-        username = datas[1]
-        password = datas[2]
-        print(f'{client_address} tried login as {username} with password {password}')
-        try:
-            with open(f'db/users/{username}') as file:
-                tru_pass = file.read().strip()  
-            if password == tru_pass:
-                client_socket.send('1'.encode())
-                print(f'{client_address} logged in successfully as {username} with password {password}')
+                current_datetime = datetime.datetime.now()
+                formatted_datetime = current_datetime.strftime('%H:%M %d.%m.%Y')
+                print(f'{formatted_datetime} | {client_address} | {data}')
+
+                datas = data.split('.')
+                cmd = datas[0]
+
+                if cmd == 'login':
+                    username = datas[1]
+                    password = datas[2]
+                    fpath = f"db/users/{username}.txt"
+                    try:
+                        with open(fpath) as file:
+                            corpass = file.read().strip()
+                        if corpass == password:
+                            client_socket.send('1'.encode('utf-8'))
+                        else:
+                            client_socket.send('2'.encode('utf-8'))
+                    except FileNotFoundError:
+                        client_socket.send('0'.encode('utf-8'))
+
+                elif cmd == "balance":
+                    username = datas[1]
+                    fpath = f'db/balance/{username}.txt'
+                    with open(fpath) as file:
+                        bala = file.read().strip()
+                    client_socket.send(bala.encode('utf-8'))
+
+                elif cmd == "transfer":
+                    froms = datas[1]
+                    amount = datas[2]
+                    amount = int(amount)
+                    to = datas[3]
+                    frompath = f'db/balance/{froms}.txt'
+                    topath = f'db/balance/{to}.txt'
+
+                    try:
+                        with open(frompath) as file:
+                            curin = file.read().strip()
+                        curin = int(curin)
+                        newbal = curin - amount
+                        newbal = str(newbal)
+                        with open(frompath, 'w') as file:
+                            file.write(newbal)
+                    except FileNotFoundError:
+                        client_socket.send('From Account Not Found'.encode('utf-8'))
+
+                    try:
+                        with open(topath) as file:
+                            curin = file.read().strip()
+                        curin = int(curin)
+                        newbal = curin + amount
+                        newbal = str(newbal)
+                        with open(topath, 'w') as file:
+                            file.write(newbal)
+                    except FileNotFoundError:
+                        client_socket.send('To Account Not Found'.encode('utf-8'))
+                    client_socket.send('Transfer Successful'.encode('utf-8'))
             else:
-                client_socket.send('0'.encode())
-                print(f'{client_address} tried incorrect password {password}')
-        except FileNotFoundError:
-            client_socket.send('2'.encode())
-            print(f'{client_address} tried account {username} {password}- Account Not Found')
-
-    if cmd == 'balance':
-        print(f'{client_address} requested Balance Mode.')
-        username = datas[1]
-        try:
-            with open(f'db/balance/{username}', 'r') as file:
-                balance = file.read().strip()
-                print(f'read balance for {client_address} as {balance}')
-            print(f'{client_address} requested balance for {username}- BALANCE RETURNED ({balance})')
-            client_socket.send(balance.encode())
-            print(f'Sent Balance {balance} to {client_address}')
-        except FileNotFoundError:
-            print(f'{client_address} requested balance for {username}- NO BALANCE FILE FOUND')
-            client_socket.send('0'.encode())
-
-    client_socket.close()
+                print('No more data from', client_address)
+                break
+    finally:
+        client_socket.close()
