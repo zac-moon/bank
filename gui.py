92e6b3dfc082bbf34a27313d8f3e4df06868a44b
zac-moon <zacemoon@gmail.com>
2023-08-17 11:55:08 +0100

    added chance error details print login error

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import socket
 
 root = tk.Tk()
 root.title('ZBANK LINK - LOGIN')
@@ -11,7 +12,18 @@ client_socket.connect(server_address)
 def main():
     username = usernameEntry.get()
     password = passwordEntry.get()
-
+    print(username)
+    print(password)
+    client_socket.send(f'login.{username}.{password}'.encode('utf-8'))
+    resp = client_socket.recv(1024)
+    resp = resp.decode('utf-8')
+    if resp == "1":
+        login = True
+        print('Correct Details')
+    elif resp == "0":
+        print('Account Not Found')    
+    else:
+        print('Password Incorrect')
 
 mainTitle = tk.Label(root, text="ZBANK LINK - LOGIN", font=('Arial', 40))
 usernameLabel = tk.Label(root, text='Username:')
