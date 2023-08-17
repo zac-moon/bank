86ef2fad7e800f8907de1d2615a5bf49862b5304
zac-moon <zacemoon@gmail.com>
2023-08-17 11:46:34 +0100

    started work on gui, base for login written correct

--- gui.py
+++ gui.py
@@ -1,3 +1,23 @@
-import socket
 import tkinter as tk
 
+root = tk.Tk()
+root.title('ZBANK LINK - LOGIN')
+root.geometry('800x600')
+
+def main():
+    print('Main')
+
+mainTitle = tk.Label(root, text="ZBANK LINK - LOGIN", font=('Arial', 40))
+usernameLabel = tk.Label(root, text='Username:')
+usernameEntry = tk.Entry(root)
+passwordLabel = tk.Label(root, text="Password:")
+passwordEntry = tk.Entry(root, show="*") 
+loginBtn = tk.Button(root, text="Login", command=main)
+
+mainTitle.pack()
+usernameLabel.pack()
+usernameEntry.pack()
+passwordLabel.pack()
+passwordEntry.pack()
+loginBtn.pack()
+root.mainloop()
